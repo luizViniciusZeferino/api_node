49e1bcfb7fe23a6b96cb54975d575d0bb4d7af1d
luizViniciusZeferino <luizvinicius1530@gmail.com>
2024-01-13 15:22:13 -0300

    controller

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const express = require('express')
+const express = require('express') // framework web p/ node.js
 const bodyParser = require ('body-parser') // converte o conteúdo para json
 
-const app = express()
-const router = express.Router()
+const app = express() // instancia do express criada e atribuida a vairiavel 
+const router = express.Router() // Este roteador pode ser usado para organizar e modularizar as rotas da aplicação.
 
-const indexRoute = require('./routes/index-route') // carregar as rotas
+const indexRoute = require('./routes/index-route') // importando as rotas 
 const productRoute = require( './routes/product-route') 
 
 app.use(bodyParser.json()) // conteúdo convertido para JSON 
 app.use(bodyParser.urlencoded({extended: false})) // codificar url's
 
-app.use ('/', indexRoute)
-app.use ('/products', productRoute)
+app.use ('/', indexRoute) // rotas associadas ao aplicativo axpress  
+app.use ('/products', productRoute) // as rotas definidas nos módulos importados estarão acessiveis nos caminhos correspondentes. 
 
 module.exports = app
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -2,32 +2,11 @@
 
 const express = require('express')
 const router = express.Router()
+const controller = require('../controllers/product-controller')
 
-router.post('/', (req, res, next) => {
-    res.status (201). send (req.body); // pegando o corpo da requisição 
-
-});
-
-router.put('/:id', (req, res, next) => { // parâmetro id 
-    const id = req.params.id // forma de recuperar parametros que irão vir pela URL 
-    res.status (200). send({
-        id: id, 
-        item: req.body
-    }); // pegando o corpo da requisição 
-});
-
-router.delete('/', (req, res, next) => {
-    res.status (200). send (req.body); // pegando o corpo da requisição 
-
-});
+router.post('/', controller.post) // delegando para o controller 
+router.put('/:id', controller.put) 
+router.delete('/', controller.delete)
 
 module.exports = router 
 
-
-/*
-const route = router.get('/', (req, res, next) => { // req requisição http, res resposta http, next  permite a passagem do controle para o próximo middleware
-    res.status (200). send ({ // enviado uma resposta 
-        title: "Node Store API", 
-        version: "0.0.1"
-    })
-});*/
