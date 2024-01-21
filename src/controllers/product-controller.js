0c41f1c4911d03d4572d5e2cd349d9c463550b6a
luizViniciusZeferino <luizvinicius1530@gmail.com>
2024-01-21 15:42:39 -0300

    listando o produto pela slug

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -15,6 +15,19 @@ exports.get = (req, res, next) => {
     })
 } 
 
+exports.getBySlug = (req, res, next) => { // fazendo um get usando como parametro o Slug do produto 
+    Product
+    .findOne({ // tras o primeiro documento que atende os critérios de consulta se não trás como array na consulta 
+        slug: req.params.slug,
+        active: true // trazer somente os itens que estão ativos / filtros
+    }, 'title description price slug tags') // buscando somento o titulo preço e item / campos que quero trazer
+    .then(data => { 
+        res.status(200).send(data) // resultado ou 
+    }).catch(e => {
+        res.status(400).send(e) // erro 
+    })
+} 
+
 exports.post = (req, res, next) => {
     let product = new Product(req.body) // instancio com o req.body, tudo que vem na requisição eu passo para o corpo do produto
     product
--- src/routes/product-route.js
+++ src/routes/product-route.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const controller = require('../controllers/product-controller')
 
 router.get('/', controller.get)// delegando para o controller
+router.get('/:slug', controller.getBySlug)
 router.post('/', controller.post)  
 router.put('/:id', controller.put) 
 router.delete('/', controller.delete)
